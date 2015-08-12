75470435963179ec0667446f5948f7273461c6ab
Jeel Patel <jeelpatel007@yahoo.com>
2015-08-12 19:40:40 -0400

    Update profanity_chk.py

--- profanity_chk.py
+++ profanity_chk.py
@@ -1,4 +1,5 @@
-import urllib
+#import urllib
+import requests
 
 # read_text parse the text file to be used for profanity check.
 
@@ -13,18 +14,20 @@ def read_text():
 # check_profanity sends the text to wdyl and returns the profanity results
 
 def check_profanity(text_to_check):
-		connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
-		output = connection.read()
-		print (output)
-		connection.close()
+		#connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+		#output = connection.read()
+		#print (output)
+		#connection.close()
+		r = requests.get('http://www.wdyl.com/profanity', params={'q': text_to_check})
 
 # display the result of our test and print a warning message if foul words are found
 
-		if "true" in output:
+		if "true" in r.text:
 			print("Profanity Alert!!")
-		elif "false" in output:
-			print ("This document has no curse words!")
+		elif "false" in r.text:
+			print ("No Profanity Found")
 		else:
 			print("Could not scan the document properly")
 		
-read_text()
+if __name__ == "__main__":
+	read_text()
